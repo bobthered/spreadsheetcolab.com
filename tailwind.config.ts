c9918c492ddcd8de85eafe7393a7a52db30ea673
Bob McAleavey <bobmcaleavey@gmail.com>
2024-10-17 09:33:45 -0400

    feat: add @bobthered/tailwindcss-palette-generator

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,16 @@
+import { tailwindcssPaletteGenerator } from '@bobthered/tailwindcss-palette-generator';
 import type { Config } from 'tailwindcss';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/sveltewind/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: tailwindcssPaletteGenerator({
+				colors: ['#54EA54', '#9aa49a'],
+				names: ['primary', 'slate']
+			})
+		}
 	},
 
 	plugins: []
